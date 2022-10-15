55ea3de8e00924e8db6799f3b46c4eff6da7e4c4
Luis Corrales <Luiscorrales2508@gmail.com>
2022-10-15 00:26:06 -0300

    intento conexion

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { GamesService } from 'src/app/services/games.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit{
+
+  constructor(private gamesService:GamesService) { }
+
+
+  ngOnInit(){
+    this.gamesService.getVideojuegos().subscribe(resp=>{
+      console.log(resp)
+    })
+  }
+
+}
